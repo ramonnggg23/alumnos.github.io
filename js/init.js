0a1c05a0293ef44720778516d0377a35aa1b8e5e
ramonnggg23 <118541724+ramonnggg23@users.noreply.github.com>
2022-11-18 07:53:56 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBOmH9jZPOvZ-yWQphw7ZuWQVOkW6eB5y0",
+  authDomain: "alumnosrrgg.firebaseapp.com",
+  projectId: "alumnosrrgg",
+  storageBucket: "alumnosrrgg.appspot.com",
+  messagingSenderId: "641829564782",
+  appId: "1:641829564782:web:ae4f9301b758190d4a917f",
+  measurementId: "G-PDB7R01546"
 });
